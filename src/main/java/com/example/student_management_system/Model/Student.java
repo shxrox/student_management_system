408371ec3bf60b36f957693e364d3d236c49e176
shxrox <sharondeva8@gmail.com>
2025-04-26 14:58:57 +0530

    Model

--- src/main/java/com/example/student_management_system/Model/Student.java
+++ src/main/java/com/example/student_management_system/Model/Student.java
@@ -1,4 +1,47 @@
 package com.example.student_management_system.Model;
 
+import jakarta.persistence.*;
+
+@Table
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(String email, long id, String name) {
+        this.email = email;
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
