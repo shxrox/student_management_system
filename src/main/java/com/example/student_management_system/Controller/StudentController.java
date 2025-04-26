0ec7dee8ba71434ffcc412c36209c163f489413f
shxrox <sharondeva8@gmail.com>
2025-04-26 15:16:00 +0530

    Student Controller

---
+++ src/main/java/com/example/student_management_system/Controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.student_management_system.Controller;
+
+import com.example.student_management_system.Model.Student;
+import com.example.student_management_system.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/student")
+    public class StudentController {
+
+        @Autowired
+        private StudentService studentService;
+
+        @GetMapping
+        public List<Student> getAllStudents() {
+            return studentService.getStudents();
+        }
+
+        @GetMapping("/{id}")
+        public Student getStudentById(@PathVariable Long id) {
+            return studentService.getStudentById(id);
+        }
+
+        @PostMapping
+        public Student addStudent(@RequestBody Student student) {
+            return studentService.saveStudent(student);
+        }
+
+        @PutMapping("/{id}")
+        public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+            return studentService.updateStudent(id, student);
+        }
+
+        @DeleteMapping("/{id}")
+        public void deleteStudent(@PathVariable Long id) {
+            studentService.deleteStudent(id);
+        }
+}
