c270555c4cd9c1cdcf4884452a55850f2ee80f8b
shxrox <sharondeva8@gmail.com>
2025-04-28 16:39:30 +0530

    Student Controller

--- src/main/java/com/example/student_management_system/Controller/StudentController.java
+++ src/main/java/com/example/student_management_system/Controller/StudentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
     @RestController
     @RequestMapping("/student")
+    @CrossOrigin(origins = "http://localhost:5173")
     public class StudentController {
 
         @Autowired
